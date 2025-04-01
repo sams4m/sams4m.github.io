02824b7a6dcab4c41916be459517aaba4083f013
Sam <kyj.samantha@gmail.com>
2025-04-01 14:08:08 +1100

    creative coding

---
+++ comm2747-cr-coding/w04-00/script.js
@@ -0,0 +1,9 @@
+document.body.style.margin = 0;
+document.body.style.overflow = `hidden`;
+
+const cnv = document.createElement("canvas");
+cnv.width = window.innerWidth;
+cnv.height = window.innerHeight;
+document.body.appendChild(cnv);
+
+const ctx = cnv.getContext("2d");
